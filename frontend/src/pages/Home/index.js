e9b316e015a80337ca76685a73698b7dbf66aab2
Brendo Gomes Santana <brendogs2021@gmail.com>
2023-06-21 16:19:31 -0300

    deixando o site responsivo

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom'
 
 import styles from './styles.module.scss'
 import ShowSlide from '../../components/ShowSlide'
+
+import { VscSourceControl } from 'react-icons/vsc'
+import { FaLaptop } from 'react-icons/fa'
+
 export default function Home() {
 
     const [Infor, setInfor] = useState('')
@@ -17,7 +21,7 @@ export default function Home() {
     },[])
     
   return (
-    <main>
+    <main className={styles.container}>
         <header className={styles.header}>
             <img src={require('../../imgs/logo.png')} alt="logo" />
             <h1>Controle de parcelas</h1>
@@ -25,10 +29,20 @@ export default function Home() {
                 <Link to='/login'>{Infor}</Link>
             </nav>
         </header>
-            <ShowSlide/>
-            <section>
-
-            </section>
+        <ShowSlide/>
+        <h2 style={{color: 'var(--branco)', textAlign: 'center', margin: '1rem 0'}}>Qual serviço oferecemos?</h2>
+        <section className={styles.Informacao}>
+           <article className={styles.card}>
+                <p className={styles.titulo}>Controle</p>
+                <VscSourceControl className={styles.imgServico}/>
+                <p>Aqui você tem o controle total do que falta pagar no seus produto.</p>
+           </article>
+           <article className={styles.card}>
+                <p className={styles.titulo}>Fácil uso</p>
+                <FaLaptop className={styles.imgServico}/>
+                <p>O site é bastante simples de utilizar, você consegue encontrar as funcionalidade de forma simples.</p>
+           </article> 
+        </section>
     </main>
   )
 }
